49728402da0a5d5d078b5344617fe64bf8ca5620
Willian Stephano <will.stephano@gmail.com>
2022-06-15 21:09:46 -0300

    Correção de erros em criação de modal

--- Js/main.js
+++ Js/main.js
@@ -1,4 +1,4 @@
-const pesquisaPais = async() => {
+const pesquisaPais = async () => {
    var pais = document.querySelector('.campo_pesquisa_txt').value
    const url = `https://restcountries.com/v3.1/name/${pais}`
    
@@ -10,7 +10,7 @@ const pesquisaPais = async() => {
       if (nomeDoPais.toUpperCase() == pais.toUpperCase()) {
          selecionaPais(nomeDoPais)
       }
-   } 
+   }
    
 }
 var campoPesquisa = document.querySelector('.campo_pesquisa_txt')
@@ -33,17 +33,17 @@ function selecionaPais(nomeDoPais) {
    const colecaoCards = document.querySelectorAll('.card_paises')
    
    for (let i = 0; i < colecaoCards.length; i++) {
-      var gambiarra = colecaoCards[i].childNodes[3].firstChild.nextSibling.textContent; 
+      var gambiarra = colecaoCards[i].childNodes[3].firstChild.nextSibling.textContent;
       if (gambiarra !== nomeDoPais) {
-         colecaoCards[i].style.display = 'none' 
-      }else{
-         colecaoCards[i].style.display = 'initial' 
+         colecaoCards[i].style.display = 'none'
+      } else {
+         colecaoCards[i].style.display = 'initial'
       }
    }
 }
 
 
-const listaPaises = async() => {
+const listaPaises = async () => {
    const url = `https://restcountries.com/v3.1/all`
    const dadosGerais = await fetch(url)
    const dadosListaPaises = await dadosGerais.json()
@@ -60,13 +60,13 @@ const listaPaises = async() => {
       let regiaoPais = card.querySelector('.regiao_pais')
       let capitalPais = card.querySelector('.capital_pais')
       
-      bandeiraPais.setAttribute('src' ,dadosListaPaises[i].flags.png)
+      bandeiraPais.setAttribute('src', dadosListaPaises[i].flags.png)
       nomePais.textContent = dadosListaPaises[i].name.common
-      populacaoPais.textContent =`População: ${dadosListaPaises[i].population}` 
+      populacaoPais.textContent = `População: ${dadosListaPaises[i].population}`
       regiaoPais.textContent = `Região: ${dadosListaPaises[i].region}`
       capitalPais.textContent = `Capital: ${dadosListaPaises[i].capital}`
       
-      abreModal(card, dadosListaPaises)
+      abreModal(card)
    }
 }
 listaPaises()
@@ -90,26 +90,26 @@ function selecionaRegiao() {
       const elementoFiltro = filtroColecao[i];
       var teste = document.querySelectorAll('.select_pais option')
       
-      elementoFiltro.addEventListener('change', function() {
+      elementoFiltro.addEventListener('change', function () {
          switch (this.value) {
             case 'af':
-            filtraRegiao(teste[1].textContent)
+               filtraRegiao(teste[1].textContent)
             break;
             
             case 'am':
-            filtraRegiao(teste[2].textContent)
+               filtraRegiao(teste[2].textContent)
             break;
             
             case 'eu':
-            filtraRegiao(teste[3].textContent)
+               filtraRegiao(teste[3].textContent)
             break;
             
             case 'oc':
-            filtraRegiao(teste[4].textContent)
+               filtraRegiao(teste[4].textContent)
             break;
             
             case 'inicial':
-            restauraPaises()
+               restauraPaises()
             break;
             
             default:
@@ -129,57 +129,61 @@ function filtraRegiao(teste) {
       
       if (gambiarraRegional !== `Região: ${teste}`) {
          colecaoCards[i].style.display = 'none'
-      }else{
-         colecaoCards[i].style.display = 'initial' 
-      } 
+      } else {
+         colecaoCards[i].style.display = 'initial'
+      }
    }
 }
 //Tenho que tentar tirar ambas gambiarras tentando pegar os elementos pelo value
 
 
-function abreModal(card, dadosListaPaises) {
+function abreModal(card) {
    //Fecha modal
-   document.querySelector('.bt_fechar_modal').addEventListener('click' ,() => {
-      divModal.classList.remove('visivel')
-   })
+   var divModal = document.querySelector(".modal");
+
+   document.querySelector(".bt_fechar_modal").addEventListener("click", () => {
+      divModal.classList.remove("visivel");
+   });
+   
    window.onclick = (event) => {
       if (event.target == divModal) {
-         divModal.classList.remove('visivel')
+         divModal.classList.remove("visivel");
       }
-   }
-
-   //Abre modal
-   var divModal = document.querySelector('.modal')
-   card.addEventListener('click' ,() => {
-      constroeModal(dadosListaPaises)
-
-      divModal.classList.toggle('visivel')
-   })   
+   };
    
+   
+   //Abre modal
+   card.addEventListener("click", async () => {
+      const title = card.children[1].children[0].textContent;
+      const pais = await fetch(`https://restcountries.com/v3.1/name/${title}`);
+      const data = await pais.json();
+      
+      constroeModal(data);
+      //console.log(Object.values(data[0].languages));
+      
+      divModal.classList.toggle("visivel");
+   });
 }
 
-function constroeModal(dadosListaPaises ,card) {
+
+function constroeModal(dadosPais) {
    var detalhesPais = document.querySelector('.detalhes_pais_modal')
-   detalhesPais.innerHTML = 
+   
+   detalhesPais.innerHTML =
    `
-      <div class="detalhes_pais_modal">
-         <div class="container">
-            <img class="flag_pais_datail" src="${dadosListaPaises[1].flags.png}" alt="">
-            <ul class="pais_detail">
-               <li class="nome_pais_detail">Name: ${dadosListaPaises[1].name.common}</li>
-               <li class="populacao_pais_detail">População: ${dadosListaPaises[1].population}</li>
-               <li class="regiao_pais_detail">Região: ${dadosListaPaises[1].region}</li>
-               <li class="subregiao_pais_detail">Sub Região: ${dadosListaPaises[1].subregion}</li>
-               <li class="capital_pais_detail">Capital: ${dadosListaPaises[1].capital}</li>
-               <li class="moeda_pais_detail">Moeda: ${dadosListaPaises[1].currencies}</li>
-               <li class="lingua_pais_detail">Linguas: ${dadosListaPaises[1].languages}</li>
-            </ul>
-         </div>
+   <div class="detalhes_pais_modal">
+      <div class="container">
+         <img class="flag_pais_datail" src="${dadosPais[0].flags.png}" alt="">
+         <ul class="pais_detail">
+            <li class="nome_pais_detail">Name: ${dadosPais[0].name.common}</li>
+            <li class="populacao_pais_detail">População: ${dadosPais[0].population}</li>
+            <li class="regiao_pais_detail">Região: ${dadosPais[0].region}</li>
+            <li class="subregiao_pais_detail">Sub Região: ${dadosPais[0].subregion}</li>
+            <li class="capital_pais_detail">Capital: ${dadosPais[0].capital}</li>
+            <li class="moeda_pais_detail">Moeda: ${Object.keys(dadosPais[0].currencies)[0]}</li>
+            <li class="lingua_pais_detail">Linguas: ${Object.values(dadosPais[0].languages)}</li>
+         </ul>
       </div>
+   </div>
    `
-   console.log(dadosListaPaises);
 }
-
-
-
-
